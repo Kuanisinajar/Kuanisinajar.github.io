0afb81868e2ce05ffb69de13c848a35a11d1bc92
Kuanisinajar <superbenben@livemail.tw>
2018-03-12 00:10:10 +0800

    Save

--- script.js
+++ script.js
@@ -78,6 +78,9 @@ day[71] = [5, true, false, false, false];
 day[72] = [5, true, false, false, false];
 day[73] = [5, true, false, false, false];
 day[74] = [5, false, false, false, false];
+day[75] = [5, true, false, false, false];
+day[76] = [5, false, false, true, false];
+day[77] = [5, false, false, true, false];
 /* ------- */
 var totalDays = day.length;
 var dotsInline = '<div class=\'dots\'></div>';

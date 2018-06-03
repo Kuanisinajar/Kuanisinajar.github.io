dc1dbc736770e89cd1f95b0bbd50407e750f4eb4
Kuanisinajar <superbenben@livemail.tw>
2018-06-03 22:30:25 +0800

    save

--- script.js
+++ script.js
@@ -164,6 +164,8 @@ day[157] = [4, false, false, false, false];
 day[158] = [6, true, false, true, true];
 day[159] = [6, false, false, true, true];
 day[160] = [7, false, false, true, true];
+day[161] = [7, false, false, true, true];
+day[162] = [5, false, false, true, false];
 /* ------- */
 var totalDays = day.length;
 var dotsInline = '<div class=\'dots\'></div>';
